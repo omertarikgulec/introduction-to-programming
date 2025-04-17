b748972266c8e45faf555853a629600283ad036b
omertarikgulec <omertarikgulec1234@gmail.com>
2025-04-18 02:21:16 +0300

    first lecture of C

--- Lecture1/welcomeToC.c
+++ Lecture1/welcomeToC.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    printf("morning : I will be sleeping at 7 p.m. so do not call me at this time\n"
-        "afternoon : I will have finished every lesson by the time end of work\n"
-        "evening : I will be watching movie but i will have slept end of day");
+    printf("welcome to C\n");
 
     return 0;
 }
