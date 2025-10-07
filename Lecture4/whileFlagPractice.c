7f0d64b6d694835e04709757e38282e9beac36e5
omertarikgulec <omertarikgulec1234@gmail.com>
2025-10-08 00:12:44 +0300

    I completed the missing parts.

--- Lecture4/whileFlagPractice.c
+++ Lecture4/whileFlagPractice.c
@@ -7,8 +7,24 @@
     int grade ;
     int totalgrade = 0 ;
 
+    printf("Enter the grade , enter -1 for exit ");
+        scanf("%d",&grade);
 
+        while(grade != -1) {
 
+            totalgrade = totalgrade + grade;
+
+            counter = counter + 1;
+
+            printf("Enter the grade , enter -1 for exit ");
+            scanf("%d",&grade);
+        }
+
+    float average = totalgrade / counter;
+
+        printf("The average is %.2f",average);
+
+        return 0;
 
 
 
